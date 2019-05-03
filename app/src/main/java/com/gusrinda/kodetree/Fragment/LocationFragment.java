34dd2089510b7f8bc9ace6cd02c1bcde458ddf82
aldyrizaldy9 <rizaldy.ilham@gmail.com>
2019-05-03 13:45:34 +0700

    maps udah dipasang (belum lokasi dari firebase

--- app/src/main/java/com/gusrinda/kodetree/Fragment/LocationFragment.java
+++ app/src/main/java/com/gusrinda/kodetree/Fragment/LocationFragment.java
@@ -1,19 +1,48 @@
 package com.gusrinda.kodetree.Fragment;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.gusrinda.kodetree.R;
 
-public class LocationFragment extends Fragment {
+public class LocationFragment extends Fragment implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_location, container, false);
+        View v = inflater.inflate(R.layout.fragment_location, container, false);
+        return v;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map1);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng ub = new LatLng(-7.952541, 112.614088);
+        mMap.addMarker(new MarkerOptions().position(ub).title("ini ub1 bngst"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(ub));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(-7.952541, 112.614088), 15.2f));
     }
 }
